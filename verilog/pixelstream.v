4864afe3768c1edf3e37e243a88e37882c504ffe
Dirk Möller <dirk.moeller@dreamchip.de>
2019-06-19 15:45:44 +0200

    development

--- verilog/pixelstream.v
+++ verilog/pixelstream.v
@@ -1,45 +1,47 @@
 module pixelstream(
-    i_clk,
-    i_reset,
+    i_wb_clk,
+    i_wb_rst,
 
-    i_pixAddr,
+    i_pixAddrBase,
+    i_pixAddrReset,
     i_pixClk,
-    i_pixGate,
+    i_pixGate, // only shift out pixels when gate is 1
     o_pixDat,
 
     o_wb_addr,
-    o_wb_sel,
     o_wb_cyc,
     i_wb_ack,
     i_wb_dat
 );
 
-input i_clk;
-input i_reset;
-
-input [31:0] i_pixAddr;
+input [31:0] i_pixAddrBase;
+input i_pixAddrReset;
 input i_pixClk;
 input i_pixGate;
 output [7:0] o_pixDat;
 
+input i_wb_clk;
+input i_wb_rst;
+input i_wb_reset;
 output [31:0] o_wb_addr;
-output [3:0] o_wb_sel;
 output o_wb_cyc;
 input i_wb_ack;
 input [31:0] i_wb_dat;
 
+//--------------------------------------
+// fifo
 
 wire fifo0_empty;
 wire fifo0_half;
 wire fifo0_full;
 
-reg fifo0_push;
-reg fifo0_pop;
+wire fifo0_push;
+wire fifo0_pop;
 wire [31:0] fifo0_dat;
 
 fifo #(.WIDTH(32), .DEPTH(4)) fifo0(
-    .i_clk(i_clk),
-    .i_reset(i_reset),
+    .i_clk(i_wb_clk),
+    .i_reset(i_wb_rst),
 
     .o_empty(fifo0_empty),
     .o_half(fifo0_half),
@@ -52,13 +54,67 @@ fifo #(.WIDTH(32), .DEPTH(4)) fifo0(
     .i_pop(fifo0_pop)
 );
 
+// start to fill up fifo when half full
+reg r_fillup;
 always @(posedge i_pixClk)
 begin
+    if( fifo0_full ) begin
+        r_fillup <= 0;
+    end
     if( fifo0_half ) begin
-
+        r_fillup <= 1;
     end
 end
 
+//-------------------------------
+// fifo gives 32 bit data
+// shifting out byte wise
+
+reg [1:0] r_idx;
+always @(posedge i_pixClk)
+begin
+    if( i_pixGate ) begin
+        r_idx <= r_idx + 'd1;
+    end
+    if( i_pixAddrReset ) begin
+        r_idx <= 0;
+    end
+end
+
+assign fifo0_pop = r_idx == 'd3;
+
+assign o_pixDat = r_idx == 0 ? fifo0_dat[31:24] :
+                  r_idx == 1 ? fifo0_dat[23:16] :
+                  r_idx == 2 ? fifo0_dat[15:8]  : fifo0_dat[7:0];
+
+
+//--------------------------------------
+// reduced wishbone interface
+
+reg [31:0] r_addr;
+
+assign o_wb_addr = r_addr;
+assign o_wb_cyc = r_fillup;
+assign fifo0_push = i_wb_ack;
+
+always @(posedge i_wb_clk)
+begin
+    if( i_wb_ack ) begin
+        
+    end
+end
+
+
+
+always @(posedge i_wb_clk)
+begin
+    if( fifo0_push ) begin
+        r_addr <= r_addr + 'd4;
+    end
+    if( i_pixAddrReset ) begin
+        r_addr <= i_pixAddrBase;
+    end
+end
 
 endmodule
 
